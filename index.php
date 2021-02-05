67bfa9a3979ec8527a38287d192a409b49229ac3
karlscheib <kes0049@mix.wvu.edu>
2021-02-05 14:17:43 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>Video Game Protagonists Inc.</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class = "card">
+                <img src="images/henry.jpeg">
+                <div class="container">
+                    <h2>Lead Director</h2>
+                    <p>Henry of Skalitz</p>
+                    <p>whereismysword@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class = "card">
+                <img src="images/doom.jpeg">
+                <div class="container">
+                    <h2>Assistant Director</h2>
+                    <p>Doomslayer</p>
+                    <p>ripandtear@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class = "card">
+                <img src="images/revenant.jpeg">
+                <div class="container">
+                    <h2>Community Manager</h2>
+                    <p>Revenant Warframe</p>
+                    <p>spin2win@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class = "card">
+                <img src="images/ryley.jpeg">
+                <div class="container">
+                    <h2>Lead Writer</h2>
+                    <p>Ryley Robinson</p>
+                    <p>swimmingisfun@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
