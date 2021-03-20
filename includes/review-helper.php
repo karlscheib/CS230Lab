0c44e19956578b92294d4f0160ab391f81ad6829
karlscheib <kes0049@mix.wvu.edu>
2021-03-19 21:23:47 -0400

    Lab 7 reviews and ratings

---
+++ includes/review-helper.php
@@ -0,0 +1,21 @@
+<?php 
+require_once 'dbhandler.php';
+date_default_timezone_set('UTC');
+
+if(isset($_POST['review-submit'])) {
+    session_start();
+    $username = $_SESSION['username'];
+    $title = $_POST['review-title'];
+    $date = date('Y-m-d H:i:s');
+    $review = $_POST['review'];
+    $item_id = $_POST['item_id'];
+    $rating = $_POST['rating'];
+
+    $sql = "INSERT INTO reviews (item_id, username, title, reviewtext, revdate, ratingnum, status) VALUES ('$item_id', '$username', '$title', '$review' , '$date', '$rating', 1);";
+    mysqli_query($conn, $sql);
+
+    header("Location: ../review.php?id=$item_id");
+    exit();
+}
+
+
